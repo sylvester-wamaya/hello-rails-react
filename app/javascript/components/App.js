730154d46ac5beec27a6ed59f2fd88ed0b81158e
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-10-03 02:40:34 +0300

    Implementredux

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Greeting from './Greeting'
+import { useDispatch } from 'react-redux'
+import { fetchGreetings } from '../redux/greetingsSlice'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchGreetings())
+  }, [dispatch])
+  
   return (
  
   <Routes>
