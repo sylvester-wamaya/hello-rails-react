ff82c3e4dd35dd7e1898dbc7e2c48725d5d2277f
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-10-03 02:41:08 +0300

    Display greetings

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Greeting = () => {
+
+  const {greetings, loading} = useSelector((store) => store.greetings)
+
+if (loading) {
+    return <div>Loading...</div>
+  }
+
   return (
+
+    
     <div>
-        This is a greeting
+      {greetings.map((greeting) => (
+        <div key={greeting.id}>{greeting.greeting}</div>
+      ))}
     </div>
   )
 }
