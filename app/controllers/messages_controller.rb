38751d4a9dd4fde326f7e61bd748ce8100bf3fe1
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-10-02 23:54:18 +0300

    Add messages controller

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,10 @@
+class MessagesController < ApplicationController
+  before_action :set_message, only: %i[ show edit update destroy ]
+
+  # GET  /messages.json
+  def index
+    random_greeting = Message.order('RANDOM()').limit(1)
+    render json: random_greeting
+  end
+end
+
